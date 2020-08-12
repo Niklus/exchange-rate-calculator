31be0a9c2b4b1a962de81454dc144147d9ab69ca
Nicholas Otieno <niklasoti@gmail.com>
2020-08-12 19:50:55 +0200

    Network Checking Fixes

--- js/networkCheck.js
+++ js/networkCheck.js
@@ -1,29 +1,36 @@
+// Network Origin and Pathname
+const origin = window.location.origin;
+const pathname = window.location.pathname;
 
 // Check for internet connection on index page
-if(window.location.pathname === '/') {
+if(pathname === '/' || pathname === '/index.html') {
+
+  const url = `${origin}/offline.html`;
 
   // If offline then navigate to offline page
   if(!window.navigator.onLine){ 
-    window.location = '/offline.html';
+    window.location.assign(url);
   }
   
   // Add event listener to navigate to offline page when no network detected
   window.addEventListener('offline', () => {
-    window.location = '/offline.html';
+    window.location.assign(url);
   });
 } 
 
 // Check for internet connection on offline page
-if(window.location.pathname === '/offline.html') {
+if(pathname === '/offline.html') {
+
+  const url = `${origin}/`;
 
   // If online then navigate to index page
   if(window.navigator.onLine){ 
-    window.location = '/';
+    window.location.assign(url);
   }
 
   // Add event listener to navigate to index page when network detected
   window.addEventListener('online', () => {
-    window.location = '/';
+    window.location.assign(url);
   })
-} 
+}
 
